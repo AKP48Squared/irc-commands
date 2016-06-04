3d1e9e134c833de727286499dedaabf0dce9424e
Alan H <feildmaster@users.noreply.github.com>
2016-06-04 10:50:36 -0500

    Check if we have the default nick when not setting a custom nick

--- commands/nick.js
+++ commands/nick.js
@@ -4,15 +4,18 @@ function Nick() {
 
 //TODO: Verify that nick is valid before attempting to set.
 Nick.prototype.respond = function (context) {
+  var IRC = context.instance;
   if(context.text) {
-    context.instance._client.send('NICK', context.text);
+    IRC._client.send('NICK', context.text);
 
     //save IRC config
-    var conf = context.instance._config;
+    var conf = IRC._config;
     conf.nick = context.text;
-    context.instance._client.opt.nick = context.text;
+    IRC._client.opt.nick = context.text;
 
     global.AKP48.saveConfig(conf, context.instanceId, true);
+  } else if (IRC._client.nick !== IRC._config.nick) {
+    IRC._client.send('NICK', IRC._config.nick);
   }
 
   return null;
