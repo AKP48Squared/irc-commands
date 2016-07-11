d18bb2f2d7350ab79bb0b63cdd04c71e49f0dfe2
Austin Peterson <austin@akpwebdesign.com>
2016-07-11 11:17:56 -0500

    nick.js text => argText

--- commands/nick.js
+++ commands/nick.js
@@ -5,21 +5,21 @@ function Nick() {
 //TODO: Verify that nick is valid before attempting to set.
 Nick.prototype.respond = function (context) {
   var IRC = context.instance();
-  if(context.text()) {
-    IRC._client.send('NICK', context.text());
+  if(context.argText()) {
+    IRC._client.send('NICK', context.argText());
 
     //wait about 2 seconds, then save IRC config, unless name wasn't changed successfully.
     setTimeout(() => {
-      if (IRC._client.nick === context.text()) {
+      if (IRC._client.nick === context.argText()) {
         //save IRC config
         var conf = IRC._config;
-        conf.nick = context.text();
-        IRC._client.opt.nick = context.text();
+        conf.nick = context.argText();
+        IRC._client.opt.nick = context.argText();
 
         global.AKP48.saveConfig(conf, context.instanceId(), true);
-        context.reply(`Nick successfully changed to "${context.text()}".`);
+        context.reply(`Nick successfully changed to "${context.argText()}".`);
       } else {
-        context.reply(`An error occurred while attempting to change nick to "${context.text()}".`);
+        context.reply(`An error occurred while attempting to change nick to "${context.argText()}".`);
       }
     }, 2000);
   } else if (IRC._client.nick !== IRC._config.nick) {
